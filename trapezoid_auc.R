eec9c9f4d7c6a89da96b44051349a8fce8174493
Benjamin Johansen <benji.johansen@gmail.com>
2024-03-05 10:49:07 +0100

    need to fix last NA col

--- trapezoid_auc.R
+++ trapezoid_auc.R
@@ -83,65 +83,74 @@ create_auc <- function(input_dataframe, max_timespan = 10, y_outcome, recorded_t
     # Create a copy of the last row with data
     last_row <- dataframe_subset %>% dplyr::slice(n())
 
+      # Set the last recorded time
+    latest_recording <- ceiling(last_row[[recorded_time]])
+
     # Set the stop criterion:
     # The minimum of either the max_timespan (function input) or the floored value of end_of_follow_up column.
     stop_criterion <- min(max_timespan, floor(last_row[[end_of_follow_up]]))
 
-    # Set the last recorded time
-    latest_recording <- ceiling(last_row[[recorded_time]])
+    if (stop_criterion > 0) {
+      # Use the interval as a counter
+      interval <- 1
 
-    # Use the interval as a counter
-    interval <- 1
-
-    # Empty vectors to hold the results
-    id_vec <- vector()
-    x_time_vec <- vector()
-    y_outcome_vec <- vector()
+      # Empty vectors to hold the results
+      id_vec <- vector()
+      x_time_vec <- vector()
+      y_outcome_vec <- vector()
 
 
-    # If no data recorded at the start, create a row to hold the first recorded value.
-    if (dataframe_subset$whole_year[1] > 0) {
-      new_row <- dataframe_subset[1, ]
-      new_row$whole_year <- 0
-      new_row[[recorded_time]] <- 0
-      dataframe_subset <- rbind(new_row, dataframe_subset)
-    }
-
-    ## Start the loop
-    # As long as the interval is less than or equal to the stop criterion, keep calculating AUC
-    while (interval <= stop_criterion) {
-      temp_auc <- dplyr::tibble()
-
-      # Subset data for the given interval
-      auc_data <- dataframe_subset %>%
-        dplyr::filter(whole_year <= interval)
-
-      # Create a copy of the last row - will be needed
-      last_auc_row <- auc_data %>% dplyr::slice(n())
-
-      # If the time of the last row is less than the current interval time and is NOT 0, then add a row
-      # to the subsetted data
-      if (last_auc_row[[recorded_time]] < interval & last_auc_row[[recorded_time]] != 0) {
-        last_auc_row[[recorded_time]] <- interval # ceiling(last_auc_row[[recorded_time]])
-        temp_auc <- rbind(auc_data, last_auc_row)
-      } else {
-        temp_auc <- auc_data
+      # If no data recorded at the start, create a row to hold the first recorded value.
+      if (dataframe_subset$whole_year[1] > 0) {
+        new_row <- dataframe_subset[1, ]
+        new_row$whole_year <- 0
+        new_row[[recorded_time]] <- 0
+        dataframe_subset <- rbind(new_row, dataframe_subset)
       }
 
-      # Calculate the AUC
-      auc <- DescTools::AUC(temp_auc[[recorded_time]], temp_auc[[y_outcome]])
+      ## Start the loop
+      # As long as the interval is less than or equal to the stop criterion, keep calculating AUC
+      while (interval <= stop_criterion) {
+        temp_auc <- dplyr::tibble()
 
-      # Add the ID, the interval and the calculated AUC to vectors
-      id_vec <- append(id_vec, id_unique)
-      x_time_vec <- append(x_time_vec, sprintf("Interval 0:%s", interval))
-      y_outcome_vec <- append(y_outcome_vec, auc)
+        # Subset data for the given interval
+        auc_data <- dataframe_subset %>%
+          dplyr::filter(whole_year <= interval)
 
-      interval <- interval + 1
+        # Create a copy of the last row - will be needed
+        last_auc_row <- auc_data %>% dplyr::slice(n())
+
+        # If the time of the last row is less than the current interval time and is NOT 0, then add a row
+        # to the subsetted data
+        if (last_auc_row[[recorded_time]] < interval & last_auc_row[[recorded_time]] != 0) {
+          last_auc_row[[recorded_time]] <- interval # ceiling(last_auc_row[[recorded_time]])
+          temp_auc <- rbind(auc_data, last_auc_row)
+        } else {
+          temp_auc <- auc_data
+        }
+
+        # Calculate the AUC
+        auc <- DescTools::AUC(temp_auc[[recorded_time]], temp_auc[[y_outcome]])
+
+        # Add the ID, the interval and the calculated AUC to vectors
+        id_vec <- append(id_vec, id_unique)
+        x_time_vec <- append(x_time_vec, sprintf("Interval 0:%s", interval))
+        y_outcome_vec <- append(y_outcome_vec, auc)
+
+        interval <- interval + 1
+      }
+
+      # Bind the outcome to the tibble when the over a subject is done
+      new_rows <- tibble(id = id_vec, x_time = x_time_vec, y_outcome = y_outcome_vec)
+      print(sprintf("ID %s done", id_unique))
+      output_tibble <- bind_rows(output_tibble, new_rows)
+
     }
-
-    # Bind the outcome to the tibble when the over a subject is done
-    new_rows <- tibble(id = id_vec, x_time = x_time_vec, y_outcome = y_outcome_vec)
-    output_tibble <- bind_rows(output_tibble, new_rows)
+    else {
+      new_rows <- tibble(id = id_unique, x_time = NA, y_outcome = NA)
+      print(sprintf("ID %s done, had empty rows", id_unique))
+      output_tibble <- bind_rows(output_tibble, new_rows)
+    }
   }
 
   return(output_tibble)
@@ -157,17 +166,6 @@ input_test <- tidyr::tibble(
   serBilir = c(1, 2, 2, 1, 1, 2, 1, 0.5)
 )
 
-test <- create_auc(input_test,
-  y_outcome = "serBilir",
-  recorded_time = "year",
-  end_of_follow_up = "years",
-  max_timespan = max_time
-)
-
-create_wide_dataframe(test, time_col = "x_time", id_col = "id", outcome_col = "y_outcome", max_timespan = max_time)
-
-# calculate AUC for T=1,2,3,4,5
-
 output_test <- data.frame(
   id = c(1, 2),
   AUC_T1 = c(1, NA),
@@ -177,16 +175,24 @@ output_test <- data.frame(
   AUC_T5 = c(6.75, NA)
 )
 
-plot(input_test$year, input_test$serBilir, col = rep(1:2))
+test <- create_auc(input_test,
+  y_outcome = "serBilir",
+  recorded_time = "year",
+  end_of_follow_up = "years",
+  max_timespan = max_time
+)
 
-plt <- ggplot(data = input_test, aes(x = year, y = serBilir, colour = as.factor(id))) +
-  geom_point(size = 5, alpha = 0.9) +
-  geom_line() +
-  ylim(0, NA) +
-  xlim(0, NA) +
-  scale_color_grey() +
-  theme_minimal()
+output <- tidyr::pivot_wider(test, names_from = x_time, values_from = y_outcome)
 
-plt
 
-ggsave(plt, filename = file.path(plots, sprintf("%s_test_cases.pdf", today())), device = "pdf")
+# Test on JMbayes
+
+test_bayes <- import_data(JMbayes2::pbc2)# %>% dplyr::filter(id == 10)
+# debugonce(create_auc)
+bayest_output <- create_auc(test_bayes,
+                                    y_outcome = "serBilir",
+                                    recorded_time = "year",
+                                    end_of_follow_up = "years",
+                                    max_timespan = max_time
+) %>%
+  tidyr::pivot_wider(., names_from = x_time, values_from = y_outcome)
